87e496305817ee32af4aad9ac542a87918602e9d
Sam <splavigne@gmail.com>
2015-01-12 12:36:20 -0500

    make graphs work again

--- bq_test.py
+++ bq_test.py
@@ -28,7 +28,7 @@ with open('config/settings.json') as json_data:
 client = get_client(settings['PROJECT_ID'], service_account=settings['SERVICE_ACCOUNT'], private_key=settings['KEY'], readonly=True)
 
 # Font variables
-FONT_LOCATION = '/Users/brianclifton/Desktop/Fonts 2/A/LL Akkurat Family OTStd/AkkuratStd-Regular.otf'
+FONT_LOCATION = 'SourceCodePro-Medium.ttf'
 font = fm.FontProperties(fname=FONT_LOCATION)
 
 # Path variables
@@ -48,7 +48,7 @@ leaders = pd.read_csv('everyone.csv')
 ##############################################################
 
 def get_leaders_from_bigquery():
-    query_template = 'SELECT SQLDATE, Actor1Name, Actor1CountryCode, GoldsteinScale, NumMentions FROM [gdelt-bq:full.events] {} AND SQLDATE>{} AND SQLDATE<{} IGNORE CASE;'
+    query_template = 'SELECT GLOBALEVENTID, SQLDATE, Actor1Name, Actor1CountryCode, GoldsteinScale, NumMentions FROM [gdelt-bq:full.events] {} AND SQLDATE>{} AND SQLDATE<{} IGNORE CASE;'
 
     date_end = ''.join(str(dt.date.today()).split('-'))
 
@@ -63,15 +63,20 @@ def get_leaders_from_bigquery():
         file_exists = os.path.isfile(CSV_PATH + filename)
 
         if file_exists:
-            date_start = get_last_row(CSV_PATH + filename)[5]
+            date_start = get_last_row(CSV_PATH + filename)[6]
         else:
             date_start = '19790101'
 
         query = query_template.format(leader['gdelt_search'], date_start, date_end)
+
+        print ''
+        print 'Search: ' + leader['display_name']
         print 'Query: ' + query
 
         try:
-            job_id, results = client.query(query, timeout=1000)
+            job_id, results = client.query(query, timeout=2000)
+            print str(len(results)) + ' results'
+
             if results == []:
                 continue
 
@@ -86,7 +91,7 @@ def get_leaders_from_bigquery():
 
             print 'Saved: ' + filename
 
-        except BigQueryTimeoutException:
+        except:
             print 'Timed out'
 
 
@@ -100,102 +105,112 @@ def get_last_row(csv_filename):
 
 def open_files():
 
-	gsCodes = pd.read_csv(GS_PATH, index_col='code', sep='\t')
+    gsCodes = pd.read_csv(GS_PATH, index_col='code', sep='\t')
 
-	for f in glob.glob(CSV_PATH + '*.csv'):
-	    perform_analysis(f, gsCodes)
+    for f in glob.glob(CSV_PATH + '*.csv'):
+        try:
+            display_name = os.path.basename(f).replace('.csv', '').replace('_', ' ')
+            leader = leaders[(leaders.display_name == display_name)]
+            perform_analysis(f, gsCodes, leader)
+        except:
+            print 'Failed to analyze: ' + f
+            continue
+
 
 ##############################################################
 
-def perform_analysis(data, gsCodes):
-	df = pd.read_csv(data, index_col='SQLDATE', parse_dates=True)
+def perform_analysis(data, gsCodes, leader):
+    df = pd.read_csv(data, index_col='SQLDATE', parse_dates=True)
 
-	name = df['Actor1Name'][0].title()
+    #name = df['Actor1Name'][0].title()
+    name = leader['display_name'].values[0]
 
-	# These steps incorporate the number of mentions a Goldstein score is associated with, reducing the impact of error in the event encoding, making the average better reflect the event's presence in the GDELT.
-	df['GoldMentions'] = df['GoldsteinScale'] * df['NumMentions']
-	goldstein = df.groupby([df.index.date]).agg({'GoldMentions': np.sum, 'NumMentions': np.sum})
-	goldstein['GoldAverage'] = goldstein['GoldMentions'] / goldstein['NumMentions']
+    # These steps incorporate the number of mentions a Goldstein score is associated with, reducing the impact of error in the event encoding, making the average better reflect the event's presence in the GDELT.
+    df['GoldMentions'] = df['GoldsteinScale'] * df['NumMentions']
+    goldstein = df.groupby([df.index.date]).agg({'GoldMentions': np.sum, 'NumMentions': np.sum})
+    goldstein['GoldAverage'] = goldstein['GoldMentions'] / goldstein['NumMentions']
 
-	full_daterange = pd.date_range(start = min(df.index), end = max(df.index))
+    full_daterange = pd.date_range(start = min(df.index), end = max(df.index))
 
-	# ffill() takes care of days that do not have a entry / Goldstein score in GDELT:
-	goldstein = goldstein.reindex(full_daterange).ffill()
+    # ffill() takes care of days that do not have a entry / Goldstein score in GDELT:
+    goldstein = goldstein.reindex(full_daterange).ffill()
 
-	# Creates a rolling_mean using a 30-day window:
-	goldstein['sma-30'] = pd.rolling_mean(goldstein['GoldAverage'], 30)
+    # Creates a rolling_mean using a 30-day window:
+    goldstein['sma-30'] = pd.rolling_mean(goldstein['GoldAverage'], 30)
 
-	# The first 30 entries in the rolling_mean become NaN, so...
-	grm = goldstein['sma-30'].dropna()
+    # The first 30 entries in the rolling_mean become NaN, so...
+    grm = goldstein['sma-30'].dropna()
 
-	test_sample = pd.DataFrame(grm)
-	test_sample.index = pd.to_datetime(test_sample.index)
-	test_sample.columns = ['Goldstein daily average']
+    test_sample = pd.DataFrame(grm)
+    test_sample.index = pd.to_datetime(test_sample.index)
+    test_sample.columns = ['Goldstein daily average']
 
-	plot_sample = pd.DataFrame(grm[-200:])
-	plot_sample.index = pd.to_datetime(plot_sample.index)
-	plot_sample.columns = ['Goldstein daily average']
+    plot_sample = pd.DataFrame(grm[-200:])
+    plot_sample.index = pd.to_datetime(plot_sample.index)
+    plot_sample.columns = ['Goldstein daily average']
 
-	# Creates the forcasting model using Autoregressive Moving Average (ARMA):
-	model = sm.tsa.ARMA(test_sample,(12, 0)).fit() # 12 Lags seems to be enough to get an accurate prediction.
+    # Creates the forcasting model using Autoregressive Moving Average (ARMA):
+    model = sm.tsa.ARMA(test_sample,(12, 0)).fit() # 12 Lags seems to be enough to get an accurate prediction.
 
-	# Creates the prediction based on the proceeding two weeks through one week into the future:
-	print twoweeksago
-	print nextweek
+    # Creates the prediction based on the proceeding two weeks through one week into the future:
+    print twoweeksago
+    print nextweek
 
-	prediction = model.predict(twoweeksago, str(nextweek), dynamic = False)
+    prediction = model.predict(twoweeksago, str(nextweek), dynamic = False)
 
-	# The fitting of the prediction to the actual looked about 1 day off, so...
-	prediction = prediction.shift(-1)
+    # The fitting of the prediction to the actual looked about 1 day off, so...
+    prediction = prediction.shift(-1)
 
-	# Finds the average of the Goldstein scores for the coming week:
-	predicts = round(prediction.ix[tomorrow:str(nextweek)].mean(), 1)
+    # Finds the average of the Goldstein scores for the coming week:
+    predicts = round(prediction.ix[tomorrow:str(nextweek)].mean(), 1)
 
-	suggestion = round(((predicts - 1) * -1), 1)
-	gsDescription = gsCodes.loc[suggestion].values[0]
+    suggestion = round(((predicts - 1) * -1), 1)
+    gsDescription = gsCodes.loc[suggestion].values[0]
 
-	print '==================='
-	print name.replace('_', ' ') + "'s Forecast: ", predicts
-	print name.replace('_', ' ') + "'s Suggested Action: ", (predicts - 1) * -1
-	print "Suggested actions for the coming week:\n" + gsDescription
-	print '==================='
+    print '==================='
+    print name + "'s Forecast: ", predicts
+    print name + "'s Suggested Action: ", (predicts - 1) * -1
+    print "Suggested actions for the coming week:\n" + gsDescription
+    print '==================='
 
-	#draw_graph(plot_sample, prediction, predicts, suggestion, name, gsDescription)
+    draw_graph(plot_sample, prediction, predicts, suggestion, name, gsDescription, leader)
 
 ##############################################################
 
-def draw_graph(plot_sample, prediction, predicts, suggestion, name, gsDescription):
+def draw_graph(plot_sample, prediction, predicts, suggestion, name, gsDescription, leader):
 
-	startdate = datetime.date(plot_sample.index[0])
-	enddate = dt.date.today() + dt.timedelta(days = 8)
-	daterange = [startdate + dt.timedelta(days = x) for x in range(0, (enddate - startdate).days)]
+    country = leader['display_country'].values[0]
+
+    startdate = datetime.date(plot_sample.index[0])
+    enddate = dt.date.today() + dt.timedelta(days = 8)
+    daterange = [startdate + dt.timedelta(days = x) for x in range(0, (enddate - startdate).days)]
 
 
-	fig = plt.figure(figsize = (14, 8))
-	ax = fig.add_subplot(111)
-	ax.set_frame_on(False)
-	plt.tick_params(axis = "both", which = "both", bottom = "off", top = "off",
-	                labelbottom = "on", left = "off", right = "off", labelleft = "on")
+    fig = plt.figure(figsize = (14, 8))
+    ax = fig.add_subplot(111)
+    ax.set_frame_on(False)
+    plt.tick_params(axis = "both", which = "both", bottom = "off", top = "off",
+                    labelbottom = "on", left = "off", right = "off", labelleft = "on")
 
-	yticks = [-10, -7.5, -5, -2.5, 0, 2.5, 5, 7.5, 10]
-	plt.yticks(yticks, alpha = 0.5, fontproperties = font, rotation = 0)
-	ax.get_yaxis().set_visible(False)
+    yticks = [-10, -7.5, -5, -2.5, 0, 2.5, 5, 7.5, 10]
+    plt.yticks(yticks, alpha = 0.5, fontproperties = font, rotation = 0)
+    ax.get_yaxis().set_visible(False)
 
-	plt.xticks(alpha = 0.5, fontproperties = font, rotation = 0)
+    plt.xticks(alpha = 0.5, fontproperties = font, rotation = 0)
 
-	blue = (26/255.,37/255.,229/255.)
-	green = (12/255., 170/255.,12/255.)
+    blue = (26/255.,37/255.,229/255.)
+    green = (12/255., 170/255.,12/255.)
 
 	# Draws the horizontal dashed lines:
-	horizontal_lines = np.linspace(-10, 10, num = 9)
-	for y in horizontal_lines:
-	    plt.plot(daterange,
-	         [y] * len(daterange),
-	         '--',
-	         lw = 0.5,
-	         color = 'black',
-	         alpha = 0.3,
-	         label = None)
+    horizontal_lines = np.linspace(-10, 10, num = 9)
+    for y in horizontal_lines:
+        plt.plot(daterange,
+                [y] * len(daterange),
+                '--',
+                lw = 0.5,
+                color = 'black',
+                alpha = 0.3,
+                label = None)
 
 	plt.plot(daterange,
 	         [1] * len(daterange),
@@ -295,8 +310,8 @@ def draw_graph(plot_sample, prediction, predicts, suggestion, name, gsDescriptio
 ##############################################################
 
 def main():
-    get_leaders_from_bigquery()
-	#open_files()
+    #get_leaders_from_bigquery()
+    open_files()
 
 
 
