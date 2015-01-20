6927a3e21c5351ce416bc8fb296c81ab7b2bffe5
Sam <splavigne@gmail.com>
2015-01-20 18:28:04 -0500

    better filter

--- filter_leaders.py
+++ filter_leaders.py
@@ -1,6 +1,7 @@
 import datetime as dt
 import pandas as pd
 import glob
+import os
 
 last_week = dt.datetime.today() - dt.timedelta(days = 7)
 
@@ -17,4 +18,14 @@ for f in glob.glob('csv/*.csv'):
 
     print f
 
+leaders = pd.read_csv('everyone.csv')
+invalid = leaders[pd.isnull(leaders['username'])]
+for row in invalid.iterrows():
+    leader = row[1]
+    print 'csv/' + leader['display_name'].replace(' ', '_') + '.csv'
 
+for f in glob.glob('csv/*.csv'):
+    display_name = os.path.basename(f).replace('.csv', '').replace('_', ' ')
+    leader = leaders[leaders['display_name'] == display_name]
+    if len(leader) == 0:
+        print f
